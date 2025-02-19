c5069e097978b0ecd43c38ab734c4e3547851fea
N'Koy OTSHUDI <nkoy.otshudi@gmail.com>
2025-02-20 00:01:04 +0100

    Revert "Mocking network requests"
    This reverts commit 2d6b5f39576ea6de1a12ed4a0cc96b9e2a70e2b8.

--- page-objects/MyAccountPage.js
+++ page-objects/MyAccountPage.js
@@ -3,7 +3,6 @@ export class MyAccountPage {
         this.page = page
 
         this.pageHeading = page.getByRole('heading', { name: 'My Account' })
-        this.errorMessage = page.locator('[data-qa="error-message"]')
     }
     
     visit = async () => {
@@ -12,9 +11,5 @@ export class MyAccountPage {
 
     waitForPageHeading = async () => {
         await this.pageHeading.waitFor()
-    }
-
-    waitForErrorMessage = async () => {
-        await this.errorMessage.waitFor()
     }
 }
