77a5498e2a13d04de5393c5496509b52ea2bcb86
N'Koy OTSHUDI <nkoy.otshudi@gmail.com>
2025-02-19 23:30:13 +0100

    skip test cookie injection

--- tests/my_account.spec.js
+++ tests/my_account.spec.js
@@ -3,7 +3,7 @@ import { MyAccountPage } from "./../page-objects/MyAccountPage.js"
 import { getLoginToken } from "./../api-calls/getLoginToken.js"
 import { adminDetails } from "./../data/userDetails.js"
 
-test.only ("My Account using cookie injection", async ({ page }) => {
+test.skip ("My Account using cookie injection", async ({ page }) => {
     // Make a request to get login token
     const loginToken = await getLoginToken(adminDetails.username, adminDetails.password)
     // Inject the login token into the browser
