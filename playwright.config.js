98c0c1617b89515382c0802dc886b9d68427ab52
N'Koy OTSHUDI <nkoy.otshudi@gmail.com>
2025-03-14 23:29:48 +0100

    chore: Update Playwright config and dependencies

--- playwright.config.js
+++ playwright.config.js
@@ -33,14 +33,11 @@ const config = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [
-    ['html'],
-    ['allure-playwright', {
-      detail: true,
-      outputFolder: 'allure-results',
-      suiteTitle: false
-    }]
-  ] : 'line',
+  reporter: [["line"], ["allure-playwright", {
+    detail: true,
+    outputFolder: 'allure-results',
+    suiteTitle: false
+  }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
