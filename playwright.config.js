3e46cdfabff999ecfdb60ad9cb2a1c03cf787285
N'Koy OTSHUDI <nkoy.otshudi@gmail.com>
2025-03-14 23:05:26 +0100

    feat: Add Allure reporting to Playwright tests

--- playwright.config.js
+++ playwright.config.js
@@ -33,7 +33,14 @@ const config = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'html' : 'line',
+  reporter: process.env.CI ? [
+    ['html'],
+    ['allure-playwright', {
+      detail: true,
+      outputFolder: 'allure-results',
+      suiteTitle: false
+    }]
+  ] : 'line',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
